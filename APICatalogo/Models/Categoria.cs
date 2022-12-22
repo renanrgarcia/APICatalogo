e19457827f7437bdda433b3fb46548ae5fe31458
Renan <renanrgarcia@gmail.com>
2022-12-22 18:26:45 -0300

    Tables relashionship definied

--- APICatalogo/Models/Categoria.cs
+++ APICatalogo/Models/Categoria.cs
@@ -2,8 +2,15 @@
 
 public class Categoria
 {
-	public int CategoriaId { get; set; }
-	public string? Nome { get; set; }
-	public string? ImageUrl { get; set; }
+    public Categoria()
+    {
+        Produtos = new Collection<Produto>();
+    }
+
+    public int CategoriaId { get; set; }
+    public string? Nome { get; set; }
+    public string? ImageUrl { get; set; }
+
+    public ICollection<Produto>? Produtos { get; set; }
 
 }
