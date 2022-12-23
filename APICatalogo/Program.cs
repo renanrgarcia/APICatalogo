9a0f6e5beef2801eafec34fa978395e4e875aa7a
Renan <renanrgarcia@gmail.com>
2022-12-23 16:12:42 -0300

    Serialize and deserialize JSON

--- APICatalogo/Program.cs
+++ APICatalogo/Program.cs
@@ -1,13 +1,16 @@
 using APICatalogo.Context;
 using Microsoft.AspNetCore.Connections;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers().AddJsonOptions(options =>
+            options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
