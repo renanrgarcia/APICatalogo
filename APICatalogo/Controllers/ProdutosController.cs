5c4a02dd6e623418af105b309dd9f9053a8c50c5
Renan <renanrgarcia@gmail.com>
2022-12-23 07:14:17 -0300

    Get All Products added

--- APICatalogo/Controllers/ProdutosController.cs
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -1,4 +1,5 @@
 ﻿using APICatalogo.Context;
+using APICatalogo.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -15,6 +16,15 @@ namespace APICatalogo.Controllers
             _context = context;
         }
 
-
+        [HttpGet]
+        public ActionResult<IEnumerable<Produto>> Get()
+        {
+            var produtos = _context.Produtos.ToList();
+            if (produtos is null)
+            {
+                return NotFound();
+            }
+            return produtos;
+        }
     }
 }
