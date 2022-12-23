dc95a1656a4c07771b3f03be57a57693d9e99f06
Renan <renanrgarcia@gmail.com>
2022-12-23 08:05:58 -0300

    Post method (Create Products) added

--- APICatalogo/Controllers/ProdutosController.cs
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -27,7 +27,7 @@ namespace APICatalogo.Controllers
             return produtos;
         }
 
-        [HttpGet("{id:int}")]
+        [HttpGet("{id:int}", Name="ObterProduto")]
         public ActionResult<Produto> Get(int id)
         {
             var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
@@ -36,7 +36,19 @@ namespace APICatalogo.Controllers
                 return NotFound("Produto não encontrado!");
             }
             return produto;
+        }
 
+        [HttpPost]
+        public ActionResult Post(Produto produto)
+        {
+            if (produto == null)
+            {
+                return BadRequest();
+            }
+            
+            _context.Produtos.Add(produto);
+            _context.SaveChanges();
+            return new CreatedAtRouteResult("ObterProduto", new { id = produto.ProdutoId}, produto);
         }
     }
 }
